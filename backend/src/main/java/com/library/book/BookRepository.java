0620a6f422e3b762f536b07217fff40f435a9e4f
masoudzadmehr <masoodzadmehr@gmail.com>
2026-07-14 08:42:27 +0330

    fix

--- backend/src/main/java/com/library/book/BookRepository.java
+++ backend/src/main/java/com/library/book/BookRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("SELECT b FROM Book b WHERE (:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))) AND (:author IS NULL OR LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%'))) AND (:isbn IS NULL OR b.isbn = :isbn)")
+    @Query("SELECT b FROM Book b WHERE (:title IS NULL OR b.title LIKE CONCAT('%', :title, '%'))) AND (:author IS NULL OR b.author LIKE CONCAT('%', :author, '%')) AND (:isbn IS NULL OR b.isbn = :isbn)")
     Page<Book> search(String title, String author, String isbn, Pageable pageable);
 
     boolean existsByIsbn(String isbn);
